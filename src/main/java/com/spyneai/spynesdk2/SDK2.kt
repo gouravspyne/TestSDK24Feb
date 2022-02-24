c56e79f08e708e34afb2dfd3b668f2e98030e270
Spyne AI <spyneai@Gouravs-M1.local>
2022-02-24 20:33:54 +0530

    first commit

---
+++ src/main/java/com/spyneai/spynesdk2/SDK2.kt
@@ -0,0 +1,11 @@
+package com.spyneai.spynesdk2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SDK2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sdk2)
+    }
+}
