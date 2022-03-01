2d4e1a4f6edb75f18adc544bab82ea60680cedf1
Spyne AI <spyneai@Gouravs-M1.local>
2022-03-01 16:42:02 +0530

    all dependencies are removed

--- src/main/java/com/spyneai/spynesdk2/SDK2.kt
+++ src/main/java/com/spyneai/spynesdk2/SDK2.kt
@@ -1,11 +1,12 @@
 package com.spyneai.spynesdk2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class SDK2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sdk2)
+
     }
 }
